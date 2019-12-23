9beb3c366995fa1f80ccedcd9b17920122ed1837
popescuciprian <popescu.ciprian98@gmail.com>
2019-12-23 13:24:37 +0000

    fml

--- server/server.js
+++ server/server.js
@@ -81,15 +81,15 @@ app.post('/:app_user/foods', async(req,res)=>{
 			    res.status(201).json({message : 'created'})
 		    }else{
                 food.appUserId=user.id;
-                await tables.Food.create(food);
-                res.status(200).json({message:'Created'});
+                await tables.Food.create(food).then(res.status(200).json({message:'Created'}));
+                // res.status(200).json({message:'Created'});
 		    }
         }
         else{
-            throw new Error();
+            throw new Error("error in creating!");
         }
     }catch(err){
-        res.status(500).json({message:err.message});
+        // res.status(500).json({message:err.message});
     }
 });
 
