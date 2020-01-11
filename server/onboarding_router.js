d5a46709f36ef9e9d52beb90216c2f167a4bedf8
popescuciprian <popescu.ciprian98@gmail.com>
2020-01-11 09:09:17 +0000

    loginRegister done

--- server/onboarding_router.js
+++ server/onboarding_router.js
@@ -8,16 +8,42 @@ router.post('/register',async (req,res)=>{
     if(req.body){
         let payload = req.body;
         let conflictUser = await tables.AppUser.findOne({
+            attributes:['username'],
             where:{
                 username:payload.username
             }
         });
         if(conflictUser){
-            res.status(400).json({message:conflictUser});
+            res.status(400).json({message:"Username allready taken!"});
         }else{
-            res.status(200).json({message:"Ready to register!"});
+            await tables.AppUser.create(payload);
+            res.status(200).json({message:`User ${payload.username} created`});
         }
     }else{
         res.status(400).json({message:"Post without body!"});
     }
 });
+
+router.get('/login',async (req,res)=>{
+    if(req.body){
+        let payload = req.body;
+        let user = await tables.AppUser.findOne({
+            where:{
+                username:payload.username
+            }
+        });
+        if(user){
+            if(user.password === payload.password){
+                res.status(200).json({message:`Authorized!`});
+            }else{
+                res.status(400).json({message:`InvalidCredentials`});
+            }
+        }else{
+                res.status(400).json({message:`InexistentUser`});
+            }
+    }else{
+        res.status(400).json({message:"Post without body!"});
+    }
+});
+
+module.exports = router;
