b191ea7b2defe33a3a97820e4af8bb1deb492340
Popescu <Stefan-Ciprian.Popescu@ing.com>
2020-01-12 09:44:53 +0200

    i need bl

--- frontend/src/AddFood.js
+++ frontend/src/AddFood.js
@@ -9,11 +9,12 @@ class AddFood extends Component {
 
     addFood() {
         let inputs = document.querySelectorAll('.add_food_container>input');
+        let select = document.querySelector('.add_food_container>select');
         let food = {
-            name:inputs[0].value,
-            exp_date:inputs[1].value,
-            availability:inputs[3].checked,
-            category:inputs[2].value,
+            name: inputs[0].value,
+            exp_date: inputs[1].value,
+            availability: inputs[2].checked,
+            category: select.value,
         }
         console.log(food);
     }
@@ -23,14 +24,20 @@ class AddFood extends Component {
             <label><b>Food Name</b></label>
             <input type="text" required />
 
+            <label><b>Category</b></label>
+            <select>
+                <option value="fruits/vegetables">fruits/vegetables</option>
+                <option value="gains/nuts">gains/nuts</option>
+                <option value="meat/seafood">meat/seafood</option>
+                <option value="dairy">dairy</option>
+                <option value="sweets">sweets</option>
+            </select>
+
             <label><b>Expiration Date</b></label>
             <input type="text" placeholder="yyyy-mm-dd" required />
 
-            <label><b>Category</b></label>
-            <input type="text" required />
-
             <label><b>Availability to share</b></label>
-            <input type="checkbox"/>
+            <input type="checkbox" />
 
             <button type="submit" onClick={this.addFood}>Add!</button>
         </div>
